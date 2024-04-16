d8b18c7a33ff0a8a80c425ba97ccfd0d6bdd0d6f
febbryandika <febbryandika97@gmail.com>
2024-04-16 13:31:13 +0700

    update script

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,8 @@ const imageHero = ["anomali-senopati-hero.jpg", "awal-mula-hero.jpg", "dailydose
 
 function changeHero() {
     const hero = document.getElementById('hero');
-    console.log(hero.src);
     const url = hero.src;
     const filename = url.substring(url.lastIndexOf('/') + 1);
-    console.log(filename);
-    console.log((imageHero.indexOf(filename) + 1) % imageHero.length);
+    
     hero.src = "./assets/img/" + imageHero[(imageHero.indexOf(filename) + 1) % imageHero.length];
-    console.log(hero.src);
 }
